0f4a03c321c9280342a44a5617b1530064eb5c89
Damien Rietdijk <damien.rietdijk@cz.nl>
2024-02-15 13:35:46 +0100

    Hello world

---
+++ dart_pattern_matching/bin/dart_pattern_matching.dart
@@ -0,0 +1,5 @@
+import 'package:dart_pattern_matching/dart_pattern_matching.dart' as dart_pattern_matching;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_pattern_matching.calculate()}!');
+}
---
+++ dart_pattern_matching/test/dart_pattern_matching_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_pattern_matching/dart_pattern_matching.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
