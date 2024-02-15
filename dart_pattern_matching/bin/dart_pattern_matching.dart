4e00bc72a0ae0f7c3a685271998099daace3a690
Damien Rietdijk <damien.rietdijk@cz.nl>
2024-02-15 16:27:04 +0100

    Test example

--- dart_pattern_matching/bin/dart_pattern_matching.dart
+++ dart_pattern_matching/bin/dart_pattern_matching.dart
@@ -1,5 +1,5 @@
 import 'package:dart_pattern_matching/dart_pattern_matching.dart' as dart_pattern_matching;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_pattern_matching.calculate()}!');
+  print('Hello world: ${dart_pattern_matching.createPerson()?.occupation}!');
 }
--- dart_pattern_matching/lib/dart_pattern_matching.dart
+++ dart_pattern_matching/lib/dart_pattern_matching.dart
@@ -1,3 +1,33 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:convert';
+
+class Person {
+  Person(
+    this.name,
+    this.age, [
+    this.occupation,
+  ]);
+
+  String name;
+  int age;
+
+  String? occupation;
+}
+
+typedef PersonMap = Map<String, Object?>;
+
+extension on PersonMap {
+  Object? get name => this['name'];
+  Object? get age => this['age'];
+  Object? get occupation => this['occupation'];
+}
+
+Person? createPerson() {
+  // final person = jsonDecode('{ "name": "Jan", "age": 8 }');
+  final person = jsonDecode('{ "name": "Jan", "age": 8, "occupation": "Developer" }');
+
+  return switch (person) {
+    PersonMap(:String name, :int age, :String? occupation) =>
+      Person(name, age, occupation),
+    _ => null,
+  };
 }
